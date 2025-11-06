6795755c9c8dd2c63afb121b802e31741cf936b4
myohtet <myohtetdev1746@gmail.com>
2025-11-06 22:27:28 +0630

    add javascript

---
+++ javascript/numbers.js
@@ -0,0 +1,23 @@
+// Positive Numbers
+console.log(13);
+console.log(99.99);
+console.log(145);
+console.log(-12);
+
+// Addition
+console.log(35 + 25);
+
+// Subtraction
+console.log(70 - 22);
+
+// Multiplication
+console.log(15 * 5);
+
+// Division
+console.log(25 / 5);
+
+// Modulus
+console.log(15 % 2);
+
+// Power
+console.log(2 ** 3);
